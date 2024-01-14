25dd5ef199580caff40dd813b36c14752f9c752a
Abhay Lodhi <156104058+vabhayv@users.noreply.github.com>
2024-01-15 00:38:15 +0530

    code for hurdle1 :)

---
+++ Hurdle 1.py
@@ -0,0 +1,18 @@
+def turn_right():
+    for left in range(3):
+        turn_left()
+def sequence():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+for i in range(6):
+    sequence()
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
